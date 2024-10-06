884e6a8cc32e8f62f5bfe7c7f1b4029f0d922a5a
Lummim Sarker <lummim@Lummims-MacBook-Pro.local>
2024-10-07 00:42:23 +0200

    vercel requirements deployedv2.0.2

--- database.py
+++ database.py
@@ -5,8 +5,9 @@ from sqlalchemy.orm import sessionmaker
 # MySQL Database URL format: mysql+pymysql://<username>:<password>@<host>/<dbname>
 
 #DATABASE_URL = "mysql+pymysql://root:@localhost:3306/fellow_1"
-DATABASE_URL = "mysql+pymysql://u767919379_rootxtech:Fello2024x@195.35.59.19:3306/u767919379_xtech"
+#DATABASE_URL = "mysql+pymysql://u767919379_rootxtech:Fello2024x@195.35.59.19:3306/u767919379_xtech"
 
+DATABASE_URL = "mysql+pymysql://u767919379_rootxtech:Fello2024x@195.35.59.19:3306/u767919379_xtech"
 
 
 engine = create_engine(DATABASE_URL)
