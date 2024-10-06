fd68353825a0e79f7016636d8de2273fb81ee4dc
Lummim Sarker <lummim@Lummims-MacBook-Pro.local>
2024-10-07 00:29:52 +0200

    updated for vercel

--- database.py
+++ database.py
@@ -3,7 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # MySQL Database URL format: mysql+pymysql://<username>:<password>@<host>/<dbname>
-DATABASE_URL = "mysql+pymysql://root:@localhost:3306/fellow_1"
+
+#DATABASE_URL = "mysql+pymysql://root:@localhost:3306/fellow_1"
+DATABASE_URL = "mysql+pymysql://u767919379_rootxtech:Fello2024x@195.35.59.19:3306/u767919379_xtech"
+
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
